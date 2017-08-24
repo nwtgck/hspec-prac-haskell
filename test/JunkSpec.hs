0f8d8663868857c30c82d7fb8a40b6ce45dd5ac9
Ryo Ota <nwtgck@gmail.com>
2017-08-25 08:50:34 +0900

    [change] Use when-False pattern to failurable tests for preparing Travis

--- test/JunkSpec.hs
+++ test/JunkSpec.hs
@@ -3,6 +3,7 @@ module JunkSpec (main, spec) where
 import Test.Hspec
 import Test.QuickCheck
 import Test.Hspec.QuickCheck
+import Control.Monad
 
 import Data.String.Strip
 
@@ -12,11 +13,13 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "int number" $ do
-    let prop_should_be_failure x = (x <= 0) ==> False
-          where types = x :: Int
-    it "should be failure(1)" $ property prop_should_be_failure
+    when False $ do
+      let prop_should_be_failure x = (x <= 0) ==> False
+            where types = x :: Int
+      it "should be failure(1)" $ property prop_should_be_failure
 
-    let prop_should_be_failure2 x = x < 30
-          where types  = x :: Int
-    modifyMaxSuccess (const 1) $
-      it "should be failure(2)" $ (property prop_should_be_failure2)
+    when False $ do
+      let prop_should_be_failure2 x = x < 30
+            where types  = x :: Int
+      modifyMaxSuccess (const 1) $
+        it "should be failure(2)" $ (property prop_should_be_failure2)
