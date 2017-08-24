c556a087af9fc1980826e9ebb66dd37fe54480b1
Ryo Ota <nwtgck@gmail.com>
2017-08-25 08:10:11 +0900

    [add] Add `modifyMaxSuccess` usage, which corresponds to `maxSuccess` in QuickCheck

--- test/Data/String/StripSpec.hs
+++ test/Data/String/StripSpec.hs
@@ -2,6 +2,7 @@ module Data.String.StripSpec (main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.Hspec.QuickCheck
 
 import Data.String.Strip
 
@@ -21,4 +22,9 @@ spec = do
   describe "int number" $ do
     let prop_should_be_failure x = (x <= 0) ==> False
           where types = x :: Int
-    it "should be failure" $ property prop_should_be_failure
+    it "should be failure(1)" $ property prop_should_be_failure
+
+    let prop_should_be_failure2 x = x < 30
+          where types  = x :: Int
+    modifyMaxSuccess (const 1) $
+      it "should be failure(2)" $ (property prop_should_be_failure2)
